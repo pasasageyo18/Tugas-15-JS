a93c7e15ee4da995369f18ad7b4e45330fc65004
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-17 11:51:26 +0700

    first commit

---
+++ tugas15.js
@@ -0,0 +1,7 @@
+let ab = 8;
+let bc = 6;
+let ac = Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2));
+
+console.log("Nilai AB :", ab);
+console.log("Nilai BC :", bc);
+console.log(`panjang sisi AC pada segitiga siku-siku tersebut adalah ${ac} cm`);
